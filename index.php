0e7f46a64448fc91045955be956b8926fa9e2ef7
COMLAB <vduay23-1833@cca.edu.ph>
2025-02-17 19:21:58 +0800

    java form

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+    <head> 
+        <title> Javascript Form </title>
+    </head>
+    
+    <body style="background-color: #040720 ; color: #fff;">
+        <h1> Student Record </h1>
+        <table id="student_table" border="1">
+        </table>
+        
+        <script>
+        <!-- GET TABLE ELEMENT -->
+            const studentTable = document
+                .getElementById("student_table");
+            
+            <!-- CREATE ROW -->
+            const row1 = studentTable.insertRow();
+            
+            <!-- CREATE 2 COLUMNS (CELL) -->
+            const row1col1 = row1.insertCell();
+            const row1col2 = row1.insertCell();
+            
+            row1col1.innerHTML = "LAST NAME";
+            row1col2.innerHTML = "FIRST NAME";
+            
+            const row2 = studentTable.insertRow();
+            const row2col1 = row2.insertCell();
+            const row2col2 = row2.insertCell();
+            
+            const inputLastName = document.createElement("input");
+            inputLastName.type="text";
+            inputLastName.placeholder="Insert Last Name";
+            inputLastName.name="last_name";
+            row2col1.append(inputLastName);
+            
+            const inputFirstName = document.createElement("input");
+            inputFirstName.type="text";
+            inputFirstName.placeholder="Insert First Name";
+            inputFirstName.name="first_name";
+            row2col2.append(inputFirstName);
+            
+            const row3 = studentTable.insertRow();
+            const row3col1 = row3.insertCell();
+            row3col1.colSpan="2";
+            
+            const inputSubmit= document.createElement("input");
+            inputSubmit.type="submit";
+            inputSubmit.value="Create Record";
+            
+            row3col1.append(inputSubmit);
+            
+            
+            const studentForm = document.createElement("form");
+            studentForm.append(studentTable);
+            studentForm.action="create.php";
+            studentForm.method="POST";
+            
+            body = document.getElementsByTagName('body')[0];
+            body.append(studentForm);
+            
+        </script>
+    </body>
+</html>
